40231a7747dabb90d13e82e19ed2b1c6469655e0
Paul Draper <paulddraper@gmail.com>
2017-12-08 03:30:06 -0700

    Collapse base command classes

--- src/main/scala/com/lucidchart/jenkins/shellnode/ShellCloudBase.scala
+++ src/main/scala/com/lucidchart/jenkins/shellnode/ShellCloudBase.scala
@@ -1,23 +1,106 @@
 package com.lucidchart.jenkins.shellnode
 
-import hudson.model.{Descriptor => HudsonDescriptor}
+import hudson.model.{Label, Node, Descriptor => HudsonDescriptor}
 import hudson.slaves.Cloud
+import hudson.slaves.NodeProvisioner.PlannedNode
+import java.io.{BufferedReader, InputStreamReader}
+import java.util.concurrent.CompletableFuture
 import jenkins.model.Jenkins
+import org.kohsuke.stapler.HttpResponses
+import resource.managed
+import scala.annotation.tailrec
 import scala.beans.BeanProperty
 import scala.collection.JavaConverters._
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Promise}
+import scala.util.control.NonFatal
+
+case class ProvisionParams(label: Label, workload: Int)
 
 class ShellCloudBase(
   @BeanProperty val command: String,
   name: String,
-  labelString: String
-) extends CommandCloudBase(name, labelString) {
+  @BeanProperty labelString: String
+) extends Cloud(name)
+    with JavaLogging {
+
+  def doProvision() = {
+    checkPermission(Cloud.PROVISION)
+    val nodeResource = for {
+      process <- run(None)
+      input <- managed(process.getInputStream)
+    } yield {
+      val reader = new BufferedReader(new InputStreamReader(input))
+      reader.readLine()
+      CustomNodeBase.XStream.fromXML(reader)
+    }
+    val node = nodeResource.acquireAndGet(_.asInstanceOf[Node])
+    Jenkins.getInstance.addNode(node)
+    HttpResponses.redirectViaContextPath(s"/computer/${node.getNodeName}")
+  }
+
+  private[this] def labels = labelString.split("\\s+")
+
+  def canProvision(label: Label) = labels.contains(label.getName)
+
+  def provision(label: Label, workload: Int) = {
+    println("provision(" + label.getName + ", " + workload + ")")
+    @tailrec
+    def next(nodes: List[PlannedNode], workload: Int): List[PlannedNode] =
+      if (workload <= 0) {
+        nodes
+      } else {
+        planned(ProvisionParams(label, workload)) match {
+          case Some(node) => next(node :: nodes, workload - node.numExecutors)
+          case None       => nodes
+        }
+      }
+    asJavaCollection(next(Nil, workload))
+  }
+
+  private[this] def planned(params: ProvisionParams) = {
+    val promise = Promise[Option[PlannedNode]]()
+    val thread = new Thread(
+      () =>
+        try {
+          for {
+            process <- run(Some(params))
+            input <- managed(process.getInputStream)
+          } {
+            process.getOutputStream.close()
+            logger.info(s"Provisioning node for ${params.label}")
+            val reader = new BufferedReader(new InputStreamReader(input))
+            val line = reader.readLine()
+            if (line == "0") {
+              promise.success(None)
+            } else {
+              val (name, capacity) = line.split(" ") match {
+                case Array(name, capacity) => name -> capacity.toInt
+              }
+              val future = new CompletableFuture[Node]
+              logger.info(s"Planned node $name for ${params.label}")
+              promise.success(Some(new PlannedNode(name, future, capacity) {
+                override def spent() = process.destroy()
+              }))
+              FutureUtil.completeWith(future)(CustomNodeBase.XStream.fromXML(reader).asInstanceOf[Node])
+              logger.info(s"Provisioned ${future.get.getNodeName} for ${params.label}")
+            }
+          }
+        } catch {
+          case NonFatal(e) => promise.tryFailure(e)
+      }
+    )
+    thread.run()
+    Await.result(promise.future, Duration.Inf)
+  }
+
   def getClouds =
     Jenkins.getInstance.clouds.asScala
       .collect { case cloud: ShellCloudBase => cloud }
       .sortBy(_.name)
       .asJava
 
-  def run(params: Option[ProvisionParams]) = ProcessUtil.runShellScript(command) { builder =>
+  private[this] def run(params: Option[ProvisionParams]) = ProcessUtil.runShellScript(command) { builder =>
     builder.environment.put("JENKINS_URL", Jenkins.getInstance.getRootUrl)
     builder.environment.put("CLOUD_NAME", name)
     params.foreach {
--- src/main/scala/com/lucidchart/jenkins/shellnode/ShellRetentionStrategyBase.scala
+++ src/main/scala/com/lucidchart/jenkins/shellnode/ShellRetentionStrategyBase.scala
@@ -2,12 +2,35 @@ package com.lucidchart.jenkins.shellnode
 
 import hudson.model.{Descriptor => HudsonDescriptor}
 import hudson.slaves.{RetentionStrategy, SlaveComputer}
+import java.io.{BufferedReader, InputStreamReader}
 import java.time.{Duration, Instant}
 import jenkins.model.Jenkins
+import resource.managed
 import scala.beans.BeanProperty
+import scala.util.control.NonFatal
 
-class ShellRetentionStrategyBase(@BeanProperty val command: String) extends CommandRetentionStrategyBase {
-  def run(computer: SlaveComputer) = ProcessUtil.runShellScript(command) { builder =>
+class ShellRetentionStrategyBase(@BeanProperty val command: String)
+    extends RetentionStrategy[SlaveComputer]
+    with JavaLogging {
+  def check(computer: SlaveComputer) =
+    try {
+      logger.info(s"Checking retention for ${computer.getName}")
+      val reader = for {
+        process <- run(computer)
+        input <- managed(process.getInputStream)
+      } yield new BufferedReader(new InputStreamReader(input))
+      reader.acquireAndGet { reader =>
+        val nextCheck = reader.readLine().toLong
+        Option(reader.readLine()).filter(_.nonEmpty).map(ComputerStatus.parse).foreach(ComputerStatus.set(computer, _))
+        Duration.ofMillis(nextCheck).toMinutes
+      }
+    } catch {
+      case NonFatal(e) =>
+        e.printStackTrace(System.err)
+        1
+    }
+
+  private[this] def run(computer: SlaveComputer) = ProcessUtil.runShellScript(command) { builder =>
     builder.environment.put("JENKINS_URL", Jenkins.getInstance.getRootUrl)
     builder.environment.put("NODE_IDLE", computer.isIdle.toString)
     builder.environment.put(
